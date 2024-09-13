96454489830069feb9a044609f97b237a69642c2
Danielius Jarmolovičius <s056908@ad.viko.lt>
2024-09-13 09:43:45 +0300

    padarytos 4 uzd

--- 1 programele/main.cpp
+++ 1 programele/main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include <string>
-using namespace std;
-int main() {
-    ///Sukurti kintamieji ir jiems priskirta informacija apie studenta
-    string vardas = "Danielius";
-    string pavarde = "Jarmolovicius";
-    int amzius = 19;
-    string grupe = "PI24";
-    int kursas = 1;
-    string studijuProgramosPavadinimas ="programu sistemos";
-/// Isvesta infromacija
 
-    cout <<"Studento informacija: " << endl;
-    cout <<"vardas: " << vardas << endl;
-    cout <<"pavarde: " << pavarde << endl;
-    cout <<"amzius: " << amzius << endl;
-    cout <<"grupe: "  << grupe << endl;
-    cout <<"kursas: "  << kursas << endl;
-    cout << "Studiju, Programos, Pavadinimas: " << studijuProgramosPavadinimas << endl;
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
-
