d35ac93e03e8832143877f5516d99e0edf60e08a
Danielius Jarmolovičius <s056908@ad.viko.lt>
2024-09-26 11:41:41 +0300

    Valiutu keitykla

---
+++ Valiutu keitykla/main.cpp
@@ -0,0 +1,25 @@
+#include <chrono>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int choise = 1;
+
+int main(){
+
+    cout << "1. Valiutos kurso palyginimas su kita valiuta" <<endl;
+    cout << "2. Pirkti valiuta" <<endl;
+    cout << "3. Parduoti valiuta" <<endl;
+    cout << "4. Iseiti" <<endl;
+    cin >> choise;
+    switch (choise) {
+        case 1:
+            cout << "Jus pasirinkote valiutos palyginima" <<endl;
+        cout << "pasirinkite valiuta kuria lyginsite su euru" <<endl;
+            cout << "1. GBP" <<endl;
+            cout << "2. USD" <<endl;
+            cout << "3. INR" <<endl;
+        cin >> ;
+
+    }
+}
